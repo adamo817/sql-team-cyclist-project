9e4cadd929e0cd6b88ab329bb4aebf2c855ff021
adamo817 <adamoyegoke3@gmail.com>
2026-01-30 15:15:57 +0200

    Update 01_create_tables.sql
    Update create tables to Portfolio schema

--- sql/01_create_tables.sql
+++ sql/01_create_tables.sql
@@ -1,1 +1,25 @@
+USE BIT_SWD03;
+GO
+
+DROP TABLE IF EXISTS CyclistPortfolio;
+DROP TABLE IF EXISTS TeamPortfolio;
+GO
+
+CREATE TABLE TeamPortfolio (
+    teamNumber INT NOT NULL,
+    teamName VARCHAR(50) NOT NULL,
+    CONSTRAINT PK_TeamPortfolio PRIMARY KEY (teamNumber)
+);
+GO
+
+CREATE TABLE CyclistPortfolio (
+    cyclistNumber INT NOT NULL,
+    familyName VARCHAR(50) NOT NULL,
+    givenName VARCHAR(50) NOT NULL,
+    teamNumber INT NOT NULL,
+    CONSTRAINT PK_CyclistPortfolio PRIMARY KEY (cyclistNumber),
+    CONSTRAINT FK_CyclistPortfolio_TeamPortfolio
+        FOREIGN KEY (teamNumber) REFERENCES TeamPortfolio(teamNumber)
+);
+GO
 
